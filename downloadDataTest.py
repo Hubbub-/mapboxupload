0494c94888439378f6fa9e39395daf7e4289318f
Reuben Poharama <reuben.hubbubstudios.com>
2016-06-03 10:04:18 +1200

    python geojson stuff

--- downloadDataTest.py
+++ downloadDataTest.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import geojson
 from pprint import pprint
 import os
 
@@ -10,5 +11,16 @@ import os
 with open('rawmessage.json') as data_file:    
     data = json.load(data_file)
 
-selectdata = data["response"]["feedMessageResponse"]["messages"]["message"][1]["dateTime"]
-pprint(selectdata)
+time = data["response"]["feedMessageResponse"]["messages"]["message"][0]["dateTime"]
+
+lat = data["response"]["feedMessageResponse"]["messages"]["message"][0]["latitude"]
+longitude = data["response"]["feedMessageResponse"]["messages"]["message"][0]["longitude"]
+
+coordinates = str(longitude) + ", " + str(lat)
+
+point1 = geojson.Point((longitude, lat))
+
+myFeature1 = geojson.Feature(geometry=point1, properties={"title": "Vlog 1", "marker-color": "#f86767", "marker-size": "large", "marker-symbol": "star", "url": "https://www.youtube.com/watch?v=ty5Cl9GbbGs"})
+
+
+pprint(myFeature1)
